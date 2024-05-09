21049f7a6b2cbc3d2659dd5870d7fd642b31812c
Shubham-Yadav14 <shubhamrao14102002@gmail.com>
2024-05-09 20:22:59 +0530

    navbar chaged

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 function Slider() {
     return (
         <>
-            <Swiper style={{ paddingTop: "0", paddingBottom: "5em" }}
+            <Swiper style={{ paddingTop: "0", paddingBottom: "5em",zIndex:"0"}}
                 modules={[Navigation, Pagination, A11y, Autoplay]}
                 spaceBetween={20}
                 slidesPerView={6}
@@ -26,7 +26,7 @@ function Slider() {
                     disableOnInteraction: false,
                 }}
             >
-                <SwiperSlide> <Card className='sliderimage' ><img className="home-slider-img" src="https://kartooncafe.com/wp-content/uploads/2023/05/cropped-Kartoon-Cafe-Final-Logo-Copy.png" alt="" /></Card></SwiperSlide>
+                <SwiperSlide > <Card className='sliderimage' ><img className="home-slider-img" src="https://kartooncafe.com/wp-content/uploads/2023/05/cropped-Kartoon-Cafe-Final-Logo-Copy.png" alt="" /></Card></SwiperSlide>
                 <SwiperSlide> <Card className='sliderimage'  ><img className="home-slider-img" src="https://ik.imagekit.io/2q7cgnqzi/Panacee%20React%20/logo/GI-Logo%20(1).png?updatedAt=1711628849372" alt="" /></Card></SwiperSlide>
                 <SwiperSlide> <Card className='sliderimage'  ><img className="home-slider-img" src="https://ik.imagekit.io/2q7cgnqzi/monogram.png?updatedAt=1710738468690" alt="" /></Card></SwiperSlide>
                 <SwiperSlide> <Card className='sliderimage' ><img className="home-slider-img" src="https://kartooncafe.com/wp-content/uploads/2023/05/cropped-Kartoon-Cafe-Final-Logo-Copy.png" alt="" /></Card></SwiperSlide>
