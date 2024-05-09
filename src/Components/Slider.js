4224862db73e521d933053814b3c9acd112a2d7a
Shubham-Yadav14 <shubhamrao14102002@gmail.com>
2024-05-09 18:27:13 +0530

    branch created

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -9,7 +9,7 @@ function Slider() {
                 modules={[Navigation, Pagination, A11y, Autoplay]}
                 spaceBetween={20}
                 slidesPerView={6}
-
+                loop={true} // Make the slider continuous
                 breakpoints={{
                     300: {
                         slidesPerView: 1,
@@ -22,7 +22,7 @@ function Slider() {
                     },
                 }}
                 autoplay={{
-                    delay: 4000,
+                    delay: 1000,
                     disableOnInteraction: false,
                 }}
             >
@@ -35,7 +35,7 @@ function Slider() {
                 <SwiperSlide> <Card className='sliderimage' ><img className="home-slider-img" src="https://kartooncafe.com/wp-content/uploads/2023/05/cropped-Kartoon-Cafe-Final-Logo-Copy.png" alt="" /></Card></SwiperSlide>
                 <SwiperSlide> <Card className='sliderimage'  ><img className="home-slider-img" src="https://ik.imagekit.io/2q7cgnqzi/Panacee%20React%20/logo/GI-Logo%20(1).png?updatedAt=1711628849372" alt="" /></Card></SwiperSlide>
                 <SwiperSlide> <Card className='sliderimage'  ><img className="home-slider-img" src="https://ik.imagekit.io/2q7cgnqzi/monogram.png?updatedAt=1710738468690" alt="" /></Card></SwiperSlide>
-               
+
             </Swiper>
 
         </>
